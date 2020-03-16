3fd7c8977b634827272d2d181a1fed69dfe57bc8
André Marques <andre.marques@metropoles.com>
2020-03-16 11:49:26 -0300

    Updates

---
+++ precache-manifest.23fd61589f52ba61470f53357bd2b599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe5cb36c13c374bffa4fc2a9e4669b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b24c7f188ce9786537",
+    "url": "/static/css/main.068397ca.chunk.css"
+  },
+  {
+    "revision": "06363bafd75fed02efdf",
+    "url": "/static/js/2.a58bbe27.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a58bbe27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b24c7f188ce9786537",
+    "url": "/static/js/main.7595e272.chunk.js"
+  },
+  {
+    "revision": "0be32c6097a5234830b9",
+    "url": "/static/js/runtime-main.ab900423.js"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/static/media/car.56be0a4e.png"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/static/media/scene.d0d2cd17.gif"
+  }
+]);
